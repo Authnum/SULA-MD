f5d9e2c97fc30f4c86f716cd22c88eb07762ad93
Authnum <125637307+Authnum@users.noreply.github.com>
2025-07-11 21:51:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=tzlRTTyb#9VpLKLJO4rOApsky1KlvA0ssa7SmDBHupbDfl_IFwzA𝐒𝐔𝐋𝐀-𝐌𝐃=tzlRTTyb#9VpLKLJO4rOApsky1KlvA0ssa7SmDBHupbDfl_IFwzA' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
