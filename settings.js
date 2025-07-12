09ac47e1b56094e41c4bb23312a1296b145dde31
Authnum <125637307+Authnum@users.noreply.github.com>
2025-07-12 08:38:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=tzlRTTyb#9VpLKLJO4rOApsky1KlvA0ssa7SmDBHupbDfl_IFwzA𝐒𝐔𝐋𝐀-𝐌𝐃=tzlRTTyb#9VpLKLJO4rOApsky1KlvA0ssa7SmDBHupbDfl_IFwzA' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
-MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
+MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "true" : process.env.AUTO_VOICE,
 AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "true" : process.env.AUTO_REPLY,
 AI_REPLAY: process.env.AI_REPLAY === undefined ? "false" : process.env.AI_REPLAY,//AI_REPLAY KARANA KOTASA ADD KARA
